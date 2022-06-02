b991bd12a518d56e0a689dba9fd7a935234dc7ee
sergkab <sergkab@gmail.com>
2022-06-02 22:49:09 +0300

    mini add admin funcs

--- djsite/mini/miniproject/blog/admin.py
+++ djsite/mini/miniproject/blog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'comtent')
+
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 
 
 
--- djsite/mini/miniproject/blog/models.py
+++ djsite/mini/miniproject/blog/models.py
@@ -8,4 +8,8 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True )
     photo = models.ImageField(upload_to='photos/%Y/%m/%d')
 
+    def __str__(self):
+        return self.title
+        
+
 
